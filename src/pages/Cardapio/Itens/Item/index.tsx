84615640044161029282ec5f88af4d7f29c14f4c
Ale <bguevaraale@gmail.com>
2023-08-29 23:21:52 -0300

    feat - adjusting styles on card

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,14 +1,15 @@
+import classNames from 'classnames';
 import React from 'react';
 import itens from '../itens.json';
 import styles from './Item.module.scss';
-type IProps = typeof itens[0]
+type IProps = typeof itens[0];
 
 export default function Item( props: IProps) {
-  const {title, description, category, size, serving, price} = props
+  const {title, description, category, size, serving, price, photo} = props
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={props.photo} alt="imagem"/>
+        <img src={photo} alt={title}/>
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
@@ -16,10 +17,10 @@ export default function Item( props: IProps) {
           <p>{ description}</p> 
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}> { category.label }</div>
-          <div className={styles.item__porcao}> { size }</div>
-          <div className={styles.item__qtdpessoas}> { serving }</div>
-          <div className={styles.item__valor}>{ price }</div>
+          <div className={classNames({ [styles.item__tipo] : true, [styles[`item__tipo__${category.label.toLowerCase()}`]]: category.label })}> { category.label }</div>
+          <div className={styles.item__porcao}> {size}g</div>
+          <div className={styles.item__qtdpessoas}> { serving } pessoa{ serving === 1 ? '' : 's'}</div>
+          <div className={styles.item__valor}>R${price.toFixed(2)}</div>
         </div>
       </div>
     </div>
